ca2043ccf87b1de977078433a0f6380de591c2fd
MisaSM <misaelsarabiamolina@gmail.com>
2026-03-12 09:09:39 -0700

    End of section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,50 +1,15 @@
-import { Box, Container, CssBaseline, Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, Container, CssBaseline } from "@mui/material";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useActivities } from "../../lib/hooks/useActivities";
+import { Outlet } from "react-router";
 
 function App() {
-  const [selectedActivityId, setSelectedActivityId] = useState<string | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const {activities, isPending} = useActivities();
-
-  const handleSelectActivityId = (id: string) => {
-    setSelectedActivityId(id);
-  };
-
-  const handleCancelSelectActivity = () => {
-    setSelectedActivityId(undefined);
-  };
-
-  const handleOpenForm = (id?: string) => {
-    if (id) handleSelectActivityId(id);
-    else handleCancelSelectActivity();
-    setEditMode(true);
-  };
-
-  const handleFormClose = () => {
-    setEditMode(false);
-  };
 
   return (
     <Box sx={{ bgColor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
-      <NavBar openForm={handleOpenForm} />
+      <NavBar />
       <Container maxWidth="xl" sx={{ marginTop: 3 }}>
-        {!activities || isPending ? (
-          <Typography>Loading...</Typography>
-        ) : (
-          <ActivityDashboard
-            activities={activities}
-            selectActivity={handleSelectActivityId}
-            cancelSelectActivity={handleCancelSelectActivity}
-            selectedActivityId={selectedActivityId}
-            editMode={editMode}
-            openForm={handleOpenForm}
-            closeForm={handleFormClose}
-          />
-        )}
+        <Outlet/>
       </Container>
     </Box>
   );
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,43 +1,15 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
-type Props = {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    cancelSelectActivity: () => void;
-    selectedActivityId?: string | undefined;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    editMode: boolean;
-}
 
-export default function ActivityDashboard({activities, 
-  cancelSelectActivity, 
-  selectActivity, 
-  selectedActivityId,
-  openForm,
-  closeForm,
-  editMode}: Props) {
-
-  const selectedActivity = activities?.find(x => x.id === selectedActivityId);
+export default function ActivityDashboard() {
 
   return (
     <Grid container spacing={3}>
       <Grid size={7}>
-        <ActivityList 
-        activities={activities} 
-        selectActivity={selectActivity}/>
+        <ActivityList />
       </Grid>
       <Grid size={5}>
-        {selectedActivity && !editMode && 
-        <ActivityDetails
-        openForm={openForm}
-        activity={selectedActivity}
-        cancelSelectActivity={cancelSelectActivity}/>}
-        {editMode && 
-        <ActivityForm closeForm={closeForm} activity={selectedActivity}/>
-        }
+        Activity filters go here        
       </Grid>
     </Grid>
   );
