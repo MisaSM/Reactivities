b622c19075094a8bb541d8c77d7af24053fa27ab
MisaSM <misaelsarabiamolina@gmail.com>
2026-03-14 10:43:39 -0700

    End of section 9

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -6,12 +6,18 @@ import {
   Typography,
   Container,
   MenuItem,
+  LinearProgress,
 } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { useStore } from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 
 export default function NavBar() {
+  const {uiStore} = useStore();
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -19,6 +25,7 @@ export default function NavBar() {
         sx={{
           backgroundImage:
             "linear-gradient(135deg, #182A73 0%, #218aae 69%, #20a7ac 89%)",
+            position: 'relative'
         }}
       >
         <Container maxWidth="xl">
@@ -36,7 +43,10 @@ export default function NavBar() {
                 Activities
                 </MenuItemLink>
                 <MenuItemLink to='/createActivity'>
-                Create Activities
+                Create Activity
+                </MenuItemLink>
+                                <MenuItemLink to='/counter'>
+                Counter
                 </MenuItemLink>
             </Box>
             <MenuItem>
@@ -44,6 +54,13 @@ export default function NavBar() {
             </MenuItem>
           </Toolbar>
         </Container>
+
+        <Observer>
+          {() => uiStore.isLoading ? (
+            <LinearProgress color='secondary' sx={{position: 'absolute', bottom: 0, left: 0, right: 0, height: 4}} />
+          ) : null}
+        </Observer>
+
       </AppBar>
     </Box>
   );
